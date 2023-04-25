6bc45c0abeced34fa2d399e305ae65f0f68698a5
irungundigirigi <irungundigirigi@gmail.com>
2023-04-26 01:23:01 +0300

    Add printf.c

---
+++ printf.c
@@ -0,0 +1,80 @@
+#include "main.h"
+/**
+  * find_function - function that finds formats for _printf
+  * calls the corresponding function.
+  * @format: format (char, string, int, decimal)
+  * Return: NULL or function associated ;
+  */
+int (*select_format(const char *format))(va_list)
+{
+	unsigned int i = 0;
+	format_sp select_f[] = {
+		{"c", write_char},
+		{"s", write_str},
+		{"i", write_int},
+		{"d", write_dec},
+		{"b", write_bin},
+		{"o", write_octal},
+		{"u", write_unsigned},
+		{NULL, NULL}
+	};
+
+	for (i = 0; select_f[i].spec; i++)
+	{
+		if (select_f[i].spec[0] == (*format))
+			return (select_f[i].func_sp);
+	}
+
+	return (NULL);
+}
+/**
+  * _printf - prints like printf function in std library
+  * @format: format specifier
+  * Return: size;
+  */
+int _printf(const char *format, ...)
+{
+    unsigned int i = 0;
+    unsigned count = 0;
+	va_list list;
+	int (*func_sp)(va_list);
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	for (i; format[i] ;)
+	{
+		for ( ; format[i] != '%' && format[i]; i++)
+		{
+			print(format[i]);
+			count++;
+		}
+		if (format[i] == '\0')
+			return (count);
+		func_sp = select_format(&format[i + 1]);
+		if (func_sp != NULL)
+		{
+			count += func_sp(list);
+			i += 2;
+			continue;
+		}
+		if (!format[i + 1])
+		{
+			return (-1);
+		}
+		print(format[i]);
+		count++;
+		if (format[i + 1] == '%')
+		{
+			i += 2;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	va_end(list);
+	return (count);
+}
